e06c2aab9b9ee0692e150877c81f5ed32bdb4d29
DiKachura <d1.princess@ya.ru>
2023-11-05 22:14:37 +0300

    счетчик

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,38 +1,76 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: UserPanel(),
+  ));
 }
 
-class MyApp extends StatelessWidget{
+class UserPanel extends StatefulWidget {
+  const UserPanel({super.key});
+
+  @override
+  State<UserPanel> createState() => _UserPanelState();
+}
+
+class _UserPanelState extends State<UserPanel> {
+
+  int _count = 0;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.deepOrangeAccent),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('DiKachura App'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Image(
-            image: AssetImage('assets/Altai-.jpg'),
-          ),
-          //child: Icon(Icons.settings_rounded, size: 45,)
-          // child: ElevatedButton.icon(
-          //   onPressed: () {},
-          //   icon: Icon(Icons.access_alarm),
-          //     label: Text('Press'),
-          // )
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () { print('clicked'); },
-          child: Text('Нажми'),
-          backgroundColor: Colors.deepOrangeAccent,
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: Text('User'),
+        centerTitle: true,
+        backgroundColor: Colors.black45,
+      ),
+      body: SafeArea(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+            children: [
+              Padding(padding: EdgeInsets.only(top: 20)),
+              Text('Diana Kachura', style: TextStyle(
+                fontSize: 25,
+                color: Colors.white
+              ),),
+              Padding(padding: EdgeInsets.only(top: 20)),
+              CircleAvatar(
+                backgroundImage: AssetImage('assets/Altai-.jpg'),
+                radius: 50,
+              ),
+              Padding(padding: EdgeInsets.only(top: 10)),
+              Row(
+                children: [
+                  Icon(Icons.mail_outline, size: 25, color: Colors.white,),
+                  Padding(padding: EdgeInsets.only(left: 10)),
+                  Text('d1.princess@ya.ru', style: TextStyle(
+                    color: Colors.white
+                  ),)
+                ],
+              ),
+              Padding(padding: EdgeInsets.only(top: 10)),
+              Text('Count: $_count', style: TextStyle(
+                  color: Colors.white
+              ),)
 
+            ],
+          )
+          ],
         ),
-      )
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.plus_one),
+          backgroundColor: Colors.amber,
+          onPressed: (){
+          setState(() {
+            _count++;
+          });
+        },
+      ),
     );
   }
 }
-
